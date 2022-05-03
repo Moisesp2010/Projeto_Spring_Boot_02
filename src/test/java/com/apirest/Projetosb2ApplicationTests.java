563319b49ac4ed5c8066e6d9f3796287c4e9dc06
MOISES PEREIRA DO NASCIMENTO <83645201+Moisesp2010@users.noreply.github.com>
2022-05-03 15:42:01 -0300

    Initial commit

---
+++ src/test/java/com/apirest/Projetosb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetosb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
